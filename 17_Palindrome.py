65c0399a5402d9815849dd41297605e39afe2311
JimcostDev <rjak727@gmail.com>
2019-12-16 19:57:21 -0500

    determinar si una palabra es palíndromo

--- 17_Palindrome.py
+++ 17_Palindrome.py
@@ -28,7 +28,7 @@ def palindrome(word):
 if __name__ == '__main__':
     word = str(input('Escribe una palabra: '))
 
-    result = palindrome(word)
+    result = palindromeTwo(word)
     if result is True:
         print('{} sí es palíndromo.'.format(word))
     else:
