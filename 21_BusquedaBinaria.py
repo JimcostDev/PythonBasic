c5cd3ebf882d02ed658ac11e7817c59a18de6c8a
JimcostDev <rjak727@gmail.com>
2020-01-29 21:53:23 -0500

    implementacion de una busqueda binaria

--- 21_BusquedaBinaria.py
+++ 21_BusquedaBinaria.py
@@ -17,7 +17,7 @@ def binary_search(numbers, number_to_find, low, high):
 if __name__ == '__main__':
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-    number_to_find = int(input('Ingresa un número: '))
+    number_to_find = int(input('Ingresa un número para buscar en la lista: '))
 
     result = binary_search(numbers, number_to_find, 0, len(numbers) - 1)
 
