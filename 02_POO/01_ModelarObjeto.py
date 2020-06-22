5b342ebb272797962937f864a5ce65a94739279c
JimcostDev <rjak727@gmail.com>
2020-06-22 16:11:53 -0500

    Programación Orientada a Objetos, Clases y Objetos

---
+++ 02_POO/01_ModelarObjeto.py
@@ -0,0 +1,37 @@
+class Lampara:
+    _LAMPS = ['''
+              .
+         .    |    ,
+          \   '   /
+           ` ,-. '
+        --- (   ) ---
+             \ /
+            _|=|_
+           |_____|
+        ''',
+              '''
+                   ,-.
+                  (   )
+                   \ /
+                  _|=|_
+                 |_____|
+              ''']
+
+    # metodo de instanciq (init, es el metodo constructor)
+    def __init__(self):
+        # por convección usamos _ para especificar que mi variable y/o metodo es de tipo privado
+        self._esta_encendida = False
+
+    def encendido(self):
+        self._esta_encendida = True
+        self._imagen_enPantalla()
+
+    def apagado(self):
+        self._esta_encendida = False
+        self._imagen_enPantalla()
+
+    def _imagen_enPantalla(self):
+        if self._esta_encendida:
+            print(self._LAMPS[0])
+        else:
+            print(self._LAMPS[1])
