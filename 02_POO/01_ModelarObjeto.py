dfd19f69a5db48c75aaf6b7fc9b3169de7393257
JimcostDev <rjak727@gmail.com>
2020-06-22 16:28:17 -0500

    Ejemplo de poo, lampara

--- 02_POO/01_ModelarObjeto.py
+++ 02_POO/01_ModelarObjeto.py
@@ -18,9 +18,9 @@ class Lampara:
               ''']
 
     # metodo de instanciq (init, es el metodo constructor)
-    def __init__(self):
+    def __init__(self, esta_encendida):
         # por convección usamos _ para especificar que mi variable y/o metodo es de tipo privado
-        self._esta_encendida = False
+        self._esta_encendida = esta_encendida
 
     def encendido(self):
         self._esta_encendida = True
@@ -35,3 +35,27 @@ class Lampara:
             print(self._LAMPS[0])
         else:
             print(self._LAMPS[1])
+
+
+def run():
+    # instancia de la clase (objeto)
+    lampara = Lampara(esta_encendida=False)
+    while True:
+        command = str(input('''
+        ¿Que deseas hacer?
+        
+        [p] prender
+        [a] apagar
+        [s] salir
+        '''))
+
+        if command == 'p':
+            lampara.encendido()
+        elif command == 'a':
+            lampara.apagado()
+        else:
+            break
+
+
+if __name__ == '__main__':
+    run()
