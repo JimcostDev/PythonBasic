6d294b42d05954028777fe22b4f4e2f038e839fd
JimcostDev <rjak727@gmail.com>
2020-06-22 15:05:34 -0500

    contar cuantas veces aparece la palabra mejor en el archivo zen

--- 30_leerArchivo.py
+++ 30_leerArchivo.py
@@ -1,8 +1,12 @@
 def run():
     with open('zen.txt') as filepy:
-        #vamos a contar cuantas veces aparece la palabra mejor en el archivo zen
+        # vamos a contar cuantas veces aparece la palabra mejor en el archivo zen
+        counter = 0
         for line in filepy:
-            print(filepy.readlines())
+            counter += line.count('mejor')
+
+        print('Mejor se encuentra {} en el texto.'.format(counter))
+
 
 if __name__ == '__main__':
     run()
